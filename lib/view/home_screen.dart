83c4cbd2c0ae898384f9fa1b7ed2ee3de08a4a6d
Truong Ngo <trg1432001.tu@gmail.com>
2024-11-15 09:41:39 +0700

    Fix home screen

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:movie_app/bloc/movie_data/movie_state.dart';
 import 'package:movie_app/data/data_model/movie.dart';
 import 'package:movie_app/view/detail_screen.dart';
 import 'package:movie_app/view/items/small_movie_card.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -20,7 +21,7 @@ class HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    _onFetchPublicMovieList(1);
+    _onFetchPublicMovieList(_page);
     _scrollController.addListener(() {
       if (_scrollController.position.atEdge) {
         bool isAtTop = _scrollController.position.pixels == 0;
@@ -30,6 +31,7 @@ class HomeScreenState extends State<HomeScreen> {
         }
       }
     });
+  }
 
   void _onFetchPublicMovieList(int page) {
     context.read<MovieBloc>().add(FetchPublicMovieList(page: page));
@@ -38,45 +40,45 @@ class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text("Movies"),
+        appBar: AppBar(
+          title: const Center(
+            child: Text("Movies"),
+          ),
         ),
-      ),
-      body: BlocBuilder<MovieBloc, MovieState>(builder: (context, state) {
-        if (state is MovieDataLoading) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (state is MovieDataLoaded) {
-          List<Movie> movieDatas = state.listMovie;
-          return Padding(
-            padding: const EdgeInsets.only(left: 24, right: 24),
-            child: ListView.builder(
-                controller: _scrollController,
-                itemCount: movieDatas.length,
-                itemBuilder: (context, index) {
-                  return SmallMovieCard(
-                    movie: movieDatas[index],
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  DetailScreen(movieId: movieDatas[index].id)));
-                    },
-                  );
-                }),
-          );
-        } else if (state is MovieDataError) {
-          return const Center(
-            child: Text("Something was wrong!"),
-          );
-        } else {
-          return const Center(
-            child: Text("No movie available!"),
-          );
-        }
-      });
+        body: BlocBuilder<MovieBloc, MovieState>(builder: (context, state) {
+          if (state is MovieDataLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is MovieDataLoaded) {
+            List<Movie> movieDatas = state.listMovie;
+            return Padding(
+              padding: const EdgeInsets.only(left: 24, right: 24),
+              child: ListView.builder(
+                  controller: _scrollController,
+                  itemCount: movieDatas.length,
+                  itemBuilder: (context, index) {
+                    return SmallMovieCard(
+                      movie: movieDatas[index],
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DetailScreen(
+                                    movieId: movieDatas[index].id)));
+                      },
+                    );
+                  }),
+            );
+          } else if (state is MovieDataError) {
+            return const Center(
+              child: Text("Something was wrong!"),
+            );
+          } else {
+            return const Center(
+              child: Text("No movie available!"),
+            );
+          }
+        }));
   }
 }
